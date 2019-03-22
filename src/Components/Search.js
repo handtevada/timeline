833025ac7cdf703cd32cb10ce7dcd3d8b1361829
Keerati Yuonghirun <algobaleno.un@gmail.com>
2019-03-22 23:38:47 +0700

    add metadata and use moment.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { Dropdown } from 'semantic-ui-react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "semantic-ui-react";
+import axios from "axios";
+import moment from 'moment';
 
-const getTimelinesList = () => axios.get('https://us-central1-timelines-48943.cloudfunctions.net/getTimelinesList');
+const getTimelinesList = () =>
+    axios.get(
+        "https://us-central1-timelines-48943.cloudfunctions.net/getTimelinesList"
+    );
 
 const Search = ({ callback }) => {
     const [option, setOption] = useState([]);
@@ -11,20 +15,23 @@ const Search = ({ callback }) => {
         getTimelinesList().then(res => {
             let options = [];
             res.data.forEach(item => {
+                let _dateEnd = item.dateEnd === "" ? new Date().toISOString().slice(0, 10) : item.dateEnd
+                let _dateEndDesc = item.dateEnd === "" ? "Present" : moment(item.dateEnd).format('YYYY MMM DD')
                 let option = {
                     key: item.name,
                     text: item.name,
-                    value: item.name + '|' + item.dateStart + '|' + (item.dateEnd === "" ? new Date().toISOString().slice(0, 10) : item.dateEnd)
+                    value: `${item.name}|${item.dateStart}|${_dateEnd}`,
+                    description: `${moment(item.dateStart).format('YYYY MMM DD')} - ${_dateEndDesc}`
                 };
                 options.push(option);
-            })
-            setOption(options)
-        })
-    }, [])
+            });
+            setOption(options);
+        });
+    }, []);
 
     return (
         <Dropdown
-            placeholder='Search'
+            placeholder="Search"
             fluid
             multiple
             search
@@ -32,7 +39,7 @@ const Search = ({ callback }) => {
             options={option}
             onChange={callback}
         />
-    )
-}
+    );
+};
 
-export default Search
+export default Search;
